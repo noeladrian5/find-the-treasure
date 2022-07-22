ca3d6cdc4f68b0099417d6f171e6c1377876022c
noeladrian5 <85713320+noeladrian5@users.noreply.github.com>
2022-07-21 21:47:34 -0500

    Create findTheTreasure.js

---
+++ findTheTreasure.js
@@ -0,0 +1,93 @@
+function moveForward(x,y, direction) {  
+    switch (direction) {
+        case 'North': {
+            startingPosition = [startingPosition[0], startingPosition[1] + 1]
+            return (startingPosition);
+        }
+        case 'South': {
+            startingPosition = [startingPosition[0], startingPosition[1] - 1]
+            return (startingPosition);
+        }
+        case 'East': {
+            startingPosition = [startingPosition[0] + 1, startingPosition[1] ]
+            return (startingPosition);
+        }
+        case 'West': {
+            startingPosition = [startingPosition[0] - 1, startingPosition[1] ]
+            return (startingPosition);
+        }
+    }
+}
+
+function turnRight(x,y, direction) {
+    switch (direction) {
+        case 'North': {
+            const newDirection = 'East';
+            return (newDirection);
+        }
+        case 'South': {
+            const newDirection = 'West';
+            return (newDirection);
+        }
+        case 'East': {
+            const newDirection = 'South';
+            return (newDirection);
+        }
+        case 'West': {
+            const newDirection = 'North';
+            return (newDirection);
+        }
+    }
+}
+
+function turnLeft(x,y, direction) {
+    switch (direction) {
+        case 'North': {
+            const newDirection = 'West';
+            return (newDirection);
+        }
+        case 'South': {
+            const newDirection = 'East';
+            return (newDirection);
+        }
+        case 'East': {
+            const newDirection = 'North';
+            return (newDirection);
+        }
+        case 'West': {
+            const newDirection = 'South';
+            return (newDirection);
+        }
+    }
+}
+
+function findTheTreasure(startingPosition, direction, instructions) {
+    instructions.forEach(element => {
+        switch (element) {
+            case 'Forward': {
+                startingPosition = moveForward(startingPosition, direction);      
+                return (startingPosition);        
+            }
+            case 'Right': {
+                direction = turnRight(direction);
+                return (direction);
+            }
+            case 'Left': {
+                direction = turnLeft(direction);
+                return (direction);
+            }
+        }
+    });
+    return ([startingPosition, direction]);   
+}
+// CTRL+D on Windows is multi select
+
+
+// SHIFT+CTRIL+ALT on windows can select multiple lines
+
+module.exports = {
+    moveForward,
+    turnLeft,
+    turnRight,
+    findTheTreasure
+}
